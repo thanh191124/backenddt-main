9b8ad5bb65540204365db5c3ff8b3b981889dc5b
thanh191124 <nguyenducthanhaz09@gmail.com>
2024-11-09 17:39:03 +0700

    Initial commit for backend

--- config/db.js
+++ config/db.js
@@ -1,16 +1,23 @@
 const mysql = require('mysql');
 
 // Cấu hình kết nối
-const dbConfig = {
-    host: 'localhost',
-    user: 'root', // Thay thế bằng username của bạn
-    password: '', // Thay thế bằng password của bạn
-    port: '3307',
-    database: 'webbandientu' // Thay thế bằng tên database bạn muốn kết nối
-};
+// const dbConfig = {
+//     host: 'localhost',
+//     user: 'root', // Thay thế bằng username của bạn
+//     password: '', // Thay thế bằng password của bạn
+//     port: '3307',
+//     database: 'webbandientu' // Thay thế bằng tên database bạn muốn kết nối
+// };
 
-// Tạo kết nối
-const connection = mysql.createConnection(dbConfig);
+// // Tạo kết nối
+// const connection = mysql.createConnection(dbConfig);
+
+var connection = mysql.createConnection({
+    host:process.env.HOST_NAME,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE
+});
 
 // Kết nối đến cơ sở dữ liệu
 connection.connect((err) => {
